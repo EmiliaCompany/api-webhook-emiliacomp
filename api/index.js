f899472040f6e077ca33d21000151f127ed01e4d
King Ard <kingard999@gmail.com>
2026-03-31 16:15:53 +0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,13 @@
+let donations = [];
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+    donations.push(data);
+    res.status(200).json({ message: 'Donation received!' });
+  } else if (req.method === 'GET') {
+    res.status(200).json(donations);
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
